9f2bb0ed7d84e3f682060ba88dd1fa9ab6ea9c26
André Machado <mtz.andremachado@gmail.com>
2021-07-17 00:49:41 -0300

    Correcao DependencyInjection

--- src/NerdStore.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.MVC/Setup/DependencyInjection.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Modulo.Catalogo.Application.Interfaces.Services;
 using NerdStore.Modulo.Catalogo.Application.Services;
 using NerdStore.Modulo.Catalogo.Data.Contexts;
 using NerdStore.Modulo.Catalogo.Data.Repository;
+using NerdStore.Modulo.Catalogo.Domain.Events;
 using NerdStore.Modulo.Catalogo.Domain.Interfaces;
 using NerdStore.Modulo.Catalogo.Domain.Services;
 using NerdStore.Shared.Bus;
@@ -20,6 +22,8 @@ namespace NerdStore.MVC.Setup
             services.AddScoped<IProdutoService, ProdutoService>();
             services.AddScoped<IEstoqueService, EstoqueService>();
             services.AddScoped<CatalogoContext>();
+
+            services.AddScoped<INotificationHandler<ProdutoEstoqueBaixoEvent>, ProdutoEventHandler>();
         }
     }
 }
